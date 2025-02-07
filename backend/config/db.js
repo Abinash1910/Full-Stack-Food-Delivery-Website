40733c2ac546ff0d1f953786dedb1124ff9fb8fd
Abinash P <145371985+Abinash1910@users.noreply.github.com>
2025-02-07 20:55:48 +0530

    Delete backend/config directory

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://greatstack:593000@cluster0.2ajk6.mongodb.net/new_food').then(()=>console.log("DB Connected"));
-}
